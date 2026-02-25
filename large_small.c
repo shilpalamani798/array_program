cd4dbb877f933ca6246c7b4f887df7982e4fa982
shilpalamani798 <shilpalamani798@gmail.com>
2026-02-25 07:20:03 +0000

    modified large and small element in array

--- large_small.c
+++ large_small.c
@@ -1,20 +1,17 @@
 #include<stdio.h>
 #include<limits.h>
-void largest(int arr[], int l)
+void large_small(int arr[], int l)
 {
-	int large=INT_MIN;
-	int small=INT_MAX;
+	int large=arr[0];
+	int small=arr[0];
 	for(int i=0;i<l;i++)
 	{
 		if(arr[i]>large)
 		large=arr[i];
+		if(arr[i]<small)
+	         small=arr[i];
 	}
         
-	for(int i=0;i<l;i++)
-        {
-                if(arr[i]<small)
-                small=arr[i];
-        }
 
 	printf("large=%d \n small=%d\n ",large,small);
  }
@@ -25,7 +22,7 @@ int main()
 {
 	int arr[]={1,43,5,78,44};
 	int l=sizeof(arr)/sizeof(arr[0]);
-	largest(arr,l);
+	large_small(arr,l);
 	
 	
 	return 0;
